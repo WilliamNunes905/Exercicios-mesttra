04585125de72c741d19bc8a69451eebcb559729c
WilliamNunes905 <williamtrybe@gmail.com>
2024-08-25 15:30:39 -0300

    ✨   Fundamentos|01 - Adicionando a instrução do exercicio

--- Exercicios/Fundamentos/Exercicio01.java
+++ Exercicios/Fundamentos/Exercicio01.java
@@ -2,6 +2,16 @@ package Exercicios.Fundamentos;
 
 import java.util.Scanner;
 
+/*
+Exercício 01: Uma imobiliária vende apenas terrenos retangulares. Faça um algoritmo para imprimir a área do terreno e o valor de venda do mesmo.
+Para isto será necessário o usuário informar as dimensões em metros (frente e lateral) do terreno além do valor cobrado pelo metro quadrado.
+
+|------- Valor de Entrada -------|
+("Informe a frente do terreno (em metros): ")
+("Informe a lateral do terreno (em metros): ")
+("Informe o valor cobrado por metro quadrado: ")
+ */
+
 public class Exercicio01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
